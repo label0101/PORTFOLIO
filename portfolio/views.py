34561f72506d66a29481bac25e3a9cc5d29000b4
muslimbek77 <muslimjonbekali@mail.ru>
2024-06-28 10:41:23 +0500

    add contact

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Contact
+from django.contrib import messages
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def home_view(request):
     return render(request,'home.html')
 
+
 def contact_view(request):
+    if request.method == 'POST':
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            content = request.POST.get('content')
+            new_contact = Contact(name=name,email=email,content=content)
+            new_contact.save()
+            messages.success(request, "Sizning xabaringiz yuborildi!!!") 
+            return HttpResponseRedirect(reverse('home-page'))
+        except:
+            pass
+
     return render(request,'contact.html')
